805d92872485f8f1a93035c0be2b5761e69bed90
az41597 <berti5105101@hotmail.com>
2024-06-05 11:54:27 +0200

    Projekti Responsive

---
+++ detyra4.py
@@ -0,0 +1,6 @@
+data = {
+    'Transaction_ID': [1, 2, 3, 4, 5],
+    'Product_Category': ['Electronics', 'Clothing', 'Electronics', 'Groceries', 'Clothing'],
+    'Amount': [500, 200, 800, 150, 300],
+    'Transaction_Date': ['2022-01-15', '2022-02-10', '2022-02-20', '2022-03-05', '2022-03-12']
+}
